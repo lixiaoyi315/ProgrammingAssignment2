1a170bbdd5023a8f2a571f4462f6926ffb307b9b
lixiaoyi315 <lixiaoyi@zbiti.com>
2016-02-11 15:40:41 +0800

    Update Matrix Inversion.R

--- Matrix Inversion.R
+++ Matrix Inversion.R
@@ -1,6 +1,8 @@
 +## Put comments here that give an overall description of what your functions do 
  +## Write a short comment describing this function 
- +
+ +##This function creates a list of functions that store and retrieve the matrix, and calculate and retrieve the inverse.
+ +##This function is the same as the example given as the only changes required were the use of 
+ +##solve instead of mean, and the limiting the argument to a Matrix.
  +makeCacheMatrix <- function(x = matrix()) { 
  +  inverse <- NULL 
  +  set <- function(y) { 
@@ -15,7 +17,11 @@
  +
  +
  +## Write a short comment describing this function 
- +
+ +##This function takes two arguments:
+ +## The first is a matrix, and the second is the list that has the cache values in it.
+ +##If the inverse has already been solved and the matrix matches the one in the cache the stored value is returned.
+ +##If the matrix has changed the new value is calculated and the cache is updated to reflect the changes.
+ +##If the value in the cache is null the values are calculated for first time and stored.
  +cacheSolve <- function(x, ...) { 
  +  ## Return a matrix that is the inverse of 'x' 
  +  inverse <- x$getinverse() 
@@ -28,5 +34,3 @@
  +  x$setinverse(inverse) 
  +  inverse 
  +} 
- +
- +# data <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow=3, ncol=3) 
